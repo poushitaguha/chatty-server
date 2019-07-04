2c8bb72d6f09254465550ed2290758dda3992915
poushitaguha <pguha.kolkata@gmail.com>
2019-07-04 21:59:54 +0000

    Display online users

--- server.js
+++ server.js
@@ -17,14 +17,30 @@ const wss = new WebSocket.Server({ server });
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
-
 wss.on('connection', function connection(ws) {
   console.log('Client connected');
+  const username = 'Anonymous';
+  
+// Create notification for user that has joined the chat
+  const newClientNotification = {
+    id: uuid(),
+    type: "incomingNotification",
+    content: "New user has joined the chat"
+  };
+
+  // Sends a notification for new client 
+  wss.clients.forEach(function each(client) {
+    client.send(JSON.stringify(newClientNotification));
+  })
+
+  // Broadcast number of clients connected to Chatty App
+  let clientCount = wss.clients.size;
+  wss.clients.forEach(function each(client) {
+    client.send(clientCount);
+  })
 
   // Broadcast messages to all clients
   ws.on('message', function incoming(data) {
-    // const msg = JSON.parse(data);
-    // console.log(msg);
 
     let receivedMsg = JSON.parse(data);
     const newMessageObject = {};
@@ -51,7 +67,6 @@ wss.on('connection', function connection(ws) {
 
     wss.clients.forEach(function each(client) {
       if (client.readyState === WebSocket.OPEN) {
-        // client.send(JSON.stringify(msg));
         client.send(JSON.stringify(newMessageObject));
       }
     });
@@ -59,7 +74,30 @@ wss.on('connection', function connection(ws) {
   });
 
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
-  ws.on('close', () => console.log('Client disconnected'));
+  // ws.on('close', () => console.log('Client disconnected'));
+  ws.on('close', () => {
+    console.log('Client disconnected')
+
+    // Create notification for user that has left the chat
+    const clientLeftNotification = {
+      id: uuid(),
+      type: "incomingNotification",
+      content: "User has left the chat"
+    };
+
+    // Sends a notification for client who has left chat 
+    wss.clients.forEach(function each(client) {
+      client.send(JSON.stringify(clientLeftNotification));
+    })
+
+    // Update number of clients connected to Chatty App
+    let clientCount = wss.clients.size;
+    wss.clients.forEach(function each(client) {
+      client.send(clientCount);
+    })
+
+  });
+
 });
 
 
